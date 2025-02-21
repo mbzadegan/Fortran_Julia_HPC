7eb99c52134fe8acefd6c1bd73791990da7cf071
Mohamad Badiezadegan <mbzadegan@gmail.com>
2025-02-20 21:18:48 -0500

    Create Simple_SUM_All_Threads.jl

---
+++ Simple_SUM_All_Threads.jl
@@ -0,0 +1,17 @@
+# Run in shell by:
+# julia -t auto Simple_SUM_All_Threads.jl
+# This command automatically uses all available threads
+
+
+
+using Base.Threads
+
+function sum_n_threads(n)
+    s = zeros(Int, nthreads())  # One sum per thread
+    @threads for i in 1:n
+        s[threadid()] += i
+    end
+    return sum(s)
+end
+
+@time sum_n_threads(10^8)
